9e80b51525965e91d0332f82405e8aeafaa293b3
HJK-G <34551787+HJK-G@users.noreply.github.com>
2021-03-04 21:32:39 -0800

    123

--- marathon.py
+++ marathon.py
@@ -1,55 +1,40 @@
 # http://www.usaco.org/index.php?page=viewproblem2&cpid=492
 
-def dist(i1, i2):
-    x1, y1 = locs[i1]
-    x2, y2 = locs[i2]
+def dist(l1, l2):
+    x1, y1 = locs[l1]
+    x2, y2 = locs[l2]
 
     return abs(x1-x2)+abs(y1-y2)
 
-def good(i, k, d):
-    if i==0:
-        return True
-    for j in range(k,K+1):
-        if 0<=disttr[i][k]<=d:
-            return False
-
-    return True
-
-def dfs(i, k, d):
-    if not good(i, k, d):
-        return
-
-    disttr[i][k] = d
-    for j in range(k,-1,-1):
-        nxt = i+j+1
-        if nxt>=N:
-            break
-        dfs(nxt, k-j, d+dist(i, nxt))
-
-
 if __name__ == '__main__':
-    fin = open('marathon.in', 'r')
-    fout = open('marathon.out', 'w')
+    # fin = open('marathon.in', 'r')
+    # fout = open('marathon.out', 'w')
 
-    # N, K = map(int, input().split())
-    N, K = map(int, fin.readline().strip().split())
+    N, K = map(int, input().split())
+    # N, K = map(int, fin.readline().strip().split())
 
     locs = []
     for i in range(N):
-        # x, y = map(int, input().split())
-        x, y = map(int, fin.readline().strip().split())
+        x, y = map(int, input().split())
+        # x, y = map(int, fin.readline().strip().split())
 
         locs.append((x, y))
 
-    disttr = [[-1]*(K+1) for i in range(N)]
-    dfs(0, K, 0)
+    dp = [[120398129501]*(K+1) for i in range(N)]
+    
+    for n in range(N):
+        for k in range(K):
+            mn = 10293120512
+            for l in range(k):
+                prv = n-l-1
+                if prv<0:
+                    break
+                dp[n][k-l] = min(dp[n][k-l], dp[prv][k-l] + dist(prv, n))
 
-    mn = 12390121024
+    mn = 21039125801240
     for i in range(K+1):
-        if disttr[N-1][i]>=0:
-            mn = min(mn, disttr[N-1][i])
-
-    # print(disttr)
-    # print(str(mn))
-    fout.write(str(mn))
-    fout.close()
+        mn = min(mn, dp[N-1][i])
+    print(dp)
+    print(str(mn))
+    # fout.write(str(mn))
+    # fout.close()
